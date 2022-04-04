afdcaf74594b310f44fc833b6622f15da517bd7c
sebif2004 <77393090+sebif2004@users.noreply.github.com>
2022-04-04 21:50:37 +0200

    Rules

--- chess.core/src/chess/core/ChessRules.java
+++ chess.core/src/chess/core/ChessRules.java
@@ -1,10 +1,48 @@
 package chess.core;
 
+import java.util.ArrayList;
+
 public class ChessRules
 {
     public ChessRules()
     {
 
     }
+    public ArrayList<ChessMove> CheckMoves(ChessPiece Piece)
+    {
+        switch(Piece)
+        {
+            case(Piece.chessPieceId==ChessPieceId.PAWN)     -> RulesPawn(Piece);
+            case(Piece.chessPieceId==ChessPieceId.TOWER)    -> RulesTower(Piece);
+            case(Piece.chessPieceId==ChessPieceId.BISHOP)   -> RulesBishop(Piece);
+            case(Piece.chessPieceId==ChessPieceId.HORSE)    -> RulesHorse(Piece);
+            case(Piece.chessPieceId==ChessPieceId.KING)     -> RulesKing(Piece);
+            case(Piece.chessPieceId==ChessPieceId.QUEEN)    -> RulesQueen(Piece);
+        }
+    }
+    public ArrayList<ChessMove> RulesPawn(ChessPiece Piece)
+    {
 
+    }
+    public ArrayList<ChessMove> RulesTower(ChessPiece Piece)
+    {
+
+    }
+    public ArrayList<ChessMove> RulesBishop(ChessPiece Piece)
+    {
+
+    }
+    public ArrayList<ChessMove> RulesHorse(ChessPiece Piece)
+    {
+
+    }
+    public ArrayList<ChessMove> RulesKing(ChessPiece Piece)
+    {
+
+    }
+    public ArrayList<ChessMove> RulesQueen(ChessPiece Piece)
+    {
+
+    }
 }
+
